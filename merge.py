767cbde516744cb1560bf666897ce4ace3213426
Philip-Hart <71290300+Philip-Hart@users.noreply.github.com>
2022-09-15 13:13:13 -0400

    different cases for saving pdf solved

--- merge.py
+++ merge.py
@@ -3,7 +3,6 @@ import os
 
 merger = PdfFileMerger()
 
-#path_to_files = r'pdfs/'
 path_to_files = input('\nInsert path to pdfs as [dir]/ (Must be in parent dir of [dir]: ')
 
 for root, dirs, file_names in os.walk(path_to_files):
@@ -13,8 +12,10 @@ for root, dirs, file_names in os.walk(path_to_files):
 
 output_path = input('\nInsert output path as [dir]/[pdf_name].pdf (Must be in parent dir of [dir]): ')
 dir_path = output_path.rsplit("/", 1)[0]
-if not os.path.exists(dir_path):
-    os.makedirs(dir_path)
+
+if "/" in output_path:
+    if not os.path.exists(dir_path) and not dir_path == '':
+        os.makedirs(dir_path)
 
 merger.write(output_path)
 merger.close
